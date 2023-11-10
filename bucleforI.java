02cc8c714d1ca8bb3167de6692b182b8a5c9ec7d
Cris <cjcris076@gmail.com>
2023-11-10 19:13:29 +0100

    MenuGit1

---
+++ bucleforI.java
@@ -0,0 +1,15 @@
+package bucleforI;
+
+public class bucleforI {
+	public static void main(String[]args) {
+		int suma=0; int i=1;
+		int num=10;
+		for( i=1; i<=12; i++) {
+			suma=num+1;
+			System.out.println(i);
+	
+		}
+		System.out.println(i);
+	}
+	
+}
