eb1fdafbdd6baac26424f8afc52c38498006e58c
CrisU <cjcris076@gmail.com>
2023-12-04 20:11:47 +0100

    Menu

---
+++ MenuCal.java
@@ -0,0 +1,28 @@
+package bucleforI;
+
+import java.util.Scanner;
+
+public class MenuCal {
+	public static void main(String[]args) {
+	Scanner sc=new Scanner(System.in);
+	int op;
+	do {
+		System.out.println("1:SUMA\n2:RESTA\n3:MULTIPLICACIÓN\n4:DIVISON\n5:SALIR");
+		op=sc.nextInt();
+		switch(op) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+			
+		}
+	}while(op!=5);
+	System.out.println("SALIENDO DEL PROGRAMA");
+	}
+}
